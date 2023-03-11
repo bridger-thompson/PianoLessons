7680d66340c362097c9f0107f9822f28e8c4333d
Bridger Thompson <bridgerat00@gmail.com>
2023-03-11 16:56:51 -0700

    Test appointment shows up

--- PianoLessons/ViewModels/SchedulePageViewModel.cs
+++ PianoLessons/ViewModels/SchedulePageViewModel.cs
@@ -2,8 +2,10 @@
 using CommunityToolkit.Mvvm.Input;
 using PianoLessons.Pages;
 using PianoLessons.Services;
+using Syncfusion.Maui.Scheduler;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,9 @@ namespace PianoLessons.ViewModels;
 public partial class SchedulePageViewModel : ObservableObject
 {
 	private readonly INavigationService navService;
+
+	[ObservableProperty]
+	private ObservableCollection<SchedulerAppointment> events;
 
 	public SchedulePageViewModel(INavigationService navService)
 	{
@@ -24,5 +29,22 @@ public partial class SchedulePageViewModel : ObservableObject
 	{
 		//nav
 		await navService.NavigateToAsync($"{nameof(AddScheduleItemPage)}");
+	}
+
+	[RelayCommand]
+	public async Task Loaded()
+	{
+		//get all from db
+		var appointment = new ObservableCollection<SchedulerAppointment>
+		{
+			new SchedulerAppointment()
+			{
+				StartTime = DateTime.Today.AddHours(9),
+				EndTime = DateTime.Today.AddHours(11),
+				Subject = "Client Meeting",
+			}
+		};
+		Events = appointment;
+
 	}
 }
