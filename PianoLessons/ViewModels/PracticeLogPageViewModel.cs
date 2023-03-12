a5db62152b2bf1378edb18fa2e7b840e4d86d333
Bridger Thompson <bridgerat00@gmail.com>
2023-03-11 19:44:18 -0700

    teacher can filter logs by student

--- PianoLessons/ViewModels/PracticeLogPageViewModel.cs
+++ PianoLessons/ViewModels/PracticeLogPageViewModel.cs
@@ -14,23 +14,44 @@ public partial class PracticeLogPageViewModel : ObservableObject
 	[ObservableProperty]
 	private ObservableCollection<PracticeLog> logs;
 
+	[ObservableProperty]
+	private List<string> students;
+
+	[ObservableProperty]
+	private string selectedStudent;
+
+	private readonly List<PracticeLog> tempLogs = new()
+	{
+		new PracticeLog()
+		{
+			Name = "Bridger",
+			Date = DateTime.Now,
+			Duration = TimeSpan.FromHours(1),
+		},
+		new PracticeLog()
+		{
+			Name = "Bridger",
+			Date = DateTime.Now,
+			Duration = TimeSpan.FromMinutes(90),
+		},
+		new PracticeLog()
+		{
+			Name = "Bob",
+			Date = DateTime.Now,
+			Duration = TimeSpan.FromMinutes(90),
+		},
+	};
+
+	[ObservableProperty]
+	private bool isTeacher;
+
+	[ObservableProperty, NotifyPropertyChangedFor(nameof(NoLogs))]
+	private bool hasLogs;
+
+	public bool NoLogs { get => !HasLogs; }
+
 	public PracticeLogPageViewModel(INavigationService navService)
 	{
-		Logs = new ObservableCollection<PracticeLog>()
-		{
-			new PracticeLog()
-			{
-				Name = "Bridger",
-				Date = DateTime.Now,
-				Duration = TimeSpan.FromHours(1),
-			},
-			new PracticeLog()
-			{
-				Name = "Bridger",
-				Date = DateTime.Now,
-				Duration = TimeSpan.FromMinutes(90),
-			},
-		};
 		this.navService = navService;
 	}
 
@@ -38,5 +59,36 @@ public partial class PracticeLogPageViewModel : ObservableObject
 	public async Task ToAddLog()
 	{
 		await navService.NavigateToAsync($"{nameof(AddLogPage)}");
+	}
+
+	[RelayCommand]
+	public async Task Loaded()
+	{
+		Students = new() {
+			"All", "Bob", "Steve", "Bridger"
+		}; ;
+		if (Students.Count > 0)
+		{
+			SelectedStudent = Students[0];
+		}
+		Logs = new();
+		//change to execute
+		await FilterLogs();
+		IsTeacher = true;
+	}
+
+	[RelayCommand]
+	public async Task FilterLogs()
+	{
+		Logs = new();
+		//replace with request to db for selected students logs
+		foreach (var log in tempLogs)
+		{
+			if (SelectedStudent == "All" || log.Name == SelectedStudent)
+			{
+				Logs.Add(log);
+			}
+		}
+		HasLogs = Logs.Count > 0;
 	}
 }
