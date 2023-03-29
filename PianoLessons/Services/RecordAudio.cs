4a9e84014308b36c8c3f761ff8f1f73fa2645ec5
Bridger Thompson <bridgerat00@gmail.com>
2023-03-29 14:47:11 -0600

    android record audio (broken)

---
+++ PianoLessons/Services/RecordAudio.cs
@@ -0,0 +1,76 @@
+﻿
+namespace PianoLessons.Services;
+
+public interface IRecordAudio
+{
+	void StartRecord();
+	string StopRecord();
+	void PauseRecord();
+	void ResetRecord();
+}
+
+public class RecordAudio : IRecordAudio
+{
+	private MediaRecorder mediaRecorder;
+	private string storagePath;
+	private bool isRecordStarted = false;
+
+	public void StartRecord()
+	{
+		if (mediaRecorder == null)
+		{
+			SetAudioFilePath();
+			mediaRecorder = new MediaRecorder();
+			mediaRecorder.Reset();
+			mediaRecorder.SetAudioSource(AudioSource.Mic);
+			mediaRecorder.SetOutputFormat(OutputFormat.AacAdts);
+			mediaRecorder.SetAudioEncoder(AudioEncoder.Aac);
+			mediaRecorder.SetOutputFile(storagePath);
+			mediaRecorder.Prepare();
+			mediaRecorder.Start();
+		}
+		else
+		{
+			mediaRecorder.Resume();
+		}
+		isRecordStarted = true;
+	}
+	public void PauseRecord()
+	{
+		if (mediaRecorder == null)
+		{
+			return;
+		}
+		mediaRecorder.Pause();
+		isRecordStarted = false;
+	}
+	public void ResetRecord()
+	{
+		if (mediaRecorder != null)
+		{
+			mediaRecorder.Resume();
+			mediaRecorder.Reset();
+		}
+		mediaRecorder = null;
+		isRecordStarted = false;
+	}
+	public string StopRecord()
+	{
+		if (mediaRecorder == null)
+		{
+			return string.Empty;
+		}
+		mediaRecorder.Resume();
+		mediaRecorder.Stop();
+		mediaRecorder = null;
+		isRecordStarted = false;
+		return storagePath;
+	}
+	private void SetAudioFilePath()
+	{
+		string fileName = "/Record_" + DateTime.UtcNow.ToString("ddMMM_hhmmss") + ".mp3";
+		var path = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+		storagePath = path + fileName;
+		Directory.CreateDirectory(path);
+	}
+}
