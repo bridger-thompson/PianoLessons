4d8582b69fa074f7927fa79ae4cf68b3ae4e3d52
Bridger Thompson <bridgerat00@gmail.com>
2023-04-06 12:44:27 -0600

    logging

--- PianoLessonsApi/Repositories/RecordingRepo.cs
+++ PianoLessonsApi/Repositories/RecordingRepo.cs
@@ -8,23 +8,28 @@ namespace PianoLessonsApi.Repositories;
 
 public class RecordingRepo
 {
-	private readonly IConfiguration config;
+	private readonly ILogger logger;
 	private BlobServiceClient client;
-	public RecordingRepo(IConfiguration config)
+	public RecordingRepo(IConfiguration config, ILogger logger)
 	{
+		this.logger = logger;
 		Uri accountUri = new Uri("https://pianorecordings.blob.core.windows.net/");
 		string accountName = config["StorageAccountName"];
 		string accountKey = config["StorageAccountAccessKey"];
+		this.logger.LogInformation(accountName, accountKey);
 		StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
 		client = new BlobServiceClient(accountUri, credential);
-		this.config = config;
 	}
 
 	public async Task SendRecordingToAzure(FileData data, string studentId)
 	{
+		logger.LogInformation($"Getting container client {studentId}");
 		BlobContainerClient containerClient = client.GetBlobContainerClient(studentId);
+		logger.LogInformation($"Done. Creating if not exist");
 		await containerClient.CreateIfNotExistsAsync();
 		MemoryStream stream = new MemoryStream(data.Data);
+		logger.LogInformation($"Uploading blob");
 		await containerClient.UploadBlobAsync(data.FileName, stream);
+		logger.LogInformation("Successfully uploaded recording");
 	}
 }
