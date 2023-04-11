b7df2a46a6ffff887466892cc60fee3670e4bf78
anthonyhardman <anthonythardman@gmail.com>
2023-04-11 16:05:28 -0600

    audio position is displayed

--- PianoLessons/Components/CustomMediaElement.xaml.cs
+++ PianoLessons/Components/CustomMediaElement.xaml.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Maui.Core.Primitives;
+using CommunityToolkit.Maui.Views;
 
 namespace PianoLessons.Components;
 
@@ -12,7 +13,6 @@ public partial class CustomMediaElement : ContentView
 		var source = (string)newValue;
 		var media = (CustomMediaElement)bindable;
 		media.mediaElement.Source = source;
-		media.duration.Text = media.mediaElement.Duration.ToString();
 	}
 
 	public string Source
@@ -20,13 +20,37 @@ public partial class CustomMediaElement : ContentView
 		get => (string)GetValue(SourceProperty);
 		set => SetValue(SourceProperty, value);
 	}
+
 	public CustomMediaElement()
 	{
 		InitializeComponent();
 		mediaElement.MediaEnded += MediaElement_MediaEnded;
+        mediaElement.PropertyChanged += MediaElement_PropertyChanged;
+		position.Text = TimeSpan.Zero.ToString("hh\\:mm\\:ss");
 	}
 
-	private void MediaElement_MediaEnded(object sender, EventArgs e)
+
+    private void Scrubber_DragStarted(object sender, EventArgs e)
+    {
+        //mediaElement.Pause();
+        Play.IsVisible = true;
+        Stop.IsVisible = false;
+    }
+
+    private void MediaElement_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == MediaElement.DurationProperty.PropertyName)
+		{
+			duration.Text = mediaElement.Duration.ToString("hh\\:mm\\:ss");
+		}
+
+		if (e.PropertyName == MediaElement.PositionProperty.PropertyName)
+		{
+			position.Text = mediaElement.Position.ToString("hh\\:mm\\:ss");
+		}
+    }
+
+    private void MediaElement_MediaEnded(object sender, EventArgs e)
 	{
 		mediaElement.Stop();
 		Play.IsVisible = true;
