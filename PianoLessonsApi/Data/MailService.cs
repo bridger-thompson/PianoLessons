01827981389d27fd40947573c941c956607032ab
Bridger Thompson <bridgerat00@gmail.com>
2023-04-20 13:09:10 -0600

    actual email now

--- PianoLessonsApi/Data/MailService.cs
+++ PianoLessonsApi/Data/MailService.cs
@@ -1,22 +1,25 @@
 ﻿using Mailjet.Client;
 using Mailjet.Client.TransactionalEmails;
 using PianoLessons.Shared.Data;
+using PianoLessonsApi.Repositories;
 
 namespace PianoLessonsApi.Data;
 
 public class MailService
 {
 	private readonly MailjetClient mailClient;
+	private readonly IPianoLessonsRepo repo;
 
-	public MailService(IConfiguration config)
+	public MailService(IConfiguration config, IPianoLessonsRepo repo)
 	{
 		mailClient = new(config["MJ_APIKEY_PUBLIC"], config["MJ_APIKEY_PRIVATE"]);
+		this.repo = repo;
 	}
 
 	public async Task SendEmail(Appointment appointment)
 	{
-		var toAddress = appointment.StudentId;
-		if (toAddress != null)
+		var student = await repo.GetStudent(appointment.StudentId);
+		if (student != null)
 		{
 			var email = new TransactionalEmailBuilder()
 				.WithFrom(new SendContact("anthony.hardman@students.snow.edu"))
@@ -27,7 +30,7 @@ public class MailService
                     <p>Your teacher scheduled a lesson with you.</p>
                     <p>Lesson: {appointment.StartAt} - {appointment.EndAt}</p>
                 ")
-				.WithTo(new SendContact(toAddress))
+				.WithTo(new SendContact(student.Email))
 				.Build();
 			var response = await mailClient.SendTransactionalEmailAsync(email);
 		}
