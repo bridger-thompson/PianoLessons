3a54c0c9261fdd634ea3fe4fa153e146d9c1dba5
Bridger Thompson <bridger.thompson@students.snow.edu>
2023-04-24 14:56:49 -0600

    tests

---
+++ PianoLessonsViewModelTests/Features/Schedule.feature
@@ -0,0 +1,10 @@
+﻿Feature: Feature1
+
+A short summary of the feature
+
+@tag1
+Scenario: Recordings add to schedule Events
+	Given a schedule view model
+	And a service that returns 2 appointments
+	When you load the page as a teacher
+	Then the schedule events should have a count of 2
---
+++ PianoLessonsViewModelTests/StepDefinitions/ScheduleStepDefinitions.cs
@@ -0,0 +1,58 @@
+using Moq;
+using PianoLessons.Services;
+using PianoLessons.Shared.Data;
+using PianoLessons.ViewModels;
+using System;
+using TechTalk.SpecFlow;
+
+namespace PianoLessonsViewModelTests.StepDefinitions
+{
+    [Binding]
+    public class ScheduleStepDefinitions
+    {
+		private readonly ScenarioContext context;
+		public ScheduleStepDefinitions(ScenarioContext context)
+		{
+			this.context = context;
+		}
+
+		[Given(@"a schedule view model")]
+        public void GivenAScheduleViewModel()
+		{
+			var mock = new Mock<IPianoLessonsService>();
+            var authMock = new Mock<IAuthService>();
+			context.Set(mock);
+            context.Set(authMock);
+			SchedulePageViewModel viewModel = new(new ShellNavigationService(), mock.Object, authMock.Object);
+            context.Set(viewModel);
+        }
+
+        [Given(@"a service that returns (.*) appointments")]
+        public void GivenAServiceThatReturnsAppointments(int p0)
+        {
+            List<Appointment> appointments = new List<Appointment>();
+            for (int i = 0; i < p0; i++) 
+            {
+                appointments.Add(new Appointment());
+            }
+			var mock = context.Get<Mock<IPianoLessonsService>>();
+            mock.Setup(x => x.GetAppointmentsForTeacher(It.IsAny<string>())).ReturnsAsync(appointments);
+        }
+
+        [When(@"you load the page as a teacher")]
+        public void WhenYouLoadThePage()
+        {
+            var authMock = context.Get<Mock<IAuthService>>();
+            authMock.Setup(x => x.User.IsTeacher).Returns(true);
+            var vm = context.Get<SchedulePageViewModel>();
+            vm.LoadedCommand.ExecuteAsync(null);
+        }
+
+        [Then(@"the schedule events should have a count of (.*)")]
+        public void ThenTheScheduleEventsShouldHaveACountOf(int p0)
+        {
+            var vm = context.Get<SchedulePageViewModel>();
+            vm.Events.Count.Should().Be(p0);
+        }
+    }
+}
